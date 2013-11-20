d56191406248a61801a716cc303ed82e152fe12d
perrie_b <jean-sebastien.perrier@epitech.eu>
2013-11-20 10:21:29 +0100

    Menu commit

--- include/MainMenuLayer.hh
+++ include/MainMenuLayer.hh
@@ -7,15 +7,24 @@
 class	MainMenuLayer : public ILayer
 {
 public:
-  MainMenuLayer(sf::RenderWindow *);
+  MainMenuLayer(sf::RenderWindow &);
   virtual ~MainMenuLayer() {}
   virtual bool update(sf::Event &);
   virtual void draw();
+  virtual void run();
 private:
-  sf::RenderWindow *	_window;
-  //sf::Sprite		_sprite;
-  //static sf::Texture	_texture;
+  sf::RenderWindow 	&_window;
+  std::vector<std::pair<sf::Texture, sf::Texture> > _textures; 
+  std::vector<std::pair<size_t, size_t> > _posText;
+  std::vector<std::pair<double,double> >_scaleText;
+  sf::Sprite	_sprite;
+  sf::Texture	_select;
+  sf::Texture	_noSelect;
   sf::Font		_font;
+  size_t	_whatButton;
+  State _state;
+  bool	_pressedDown;
+  bool _pressedUp;
 };
 
 #endif	// _MAINMENULAYER_HH_
